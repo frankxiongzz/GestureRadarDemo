35b2a3b7f72353651677339513e946cd9a966735
frank.xiong <frank.xiong@timevary.com>
2020-02-21 13:52:59 +0800

    update new head file

--- GestureRadarDemo/GestureRadarDemo.cpp
+++ GestureRadarDemo/GestureRadarDemo.cpp
@@ -11,58 +11,33 @@
 using namespace std;
 
 #pragma comment(lib,"GestureRadarSDK.lib")  
-
-
-int recvRAW(int x, int y, int reserve, int z)
+int recvRAW(float x, float y, float z)
 {
-	cout << x << "," << y << "," << z << endl;
+	//cout <<" Data:" << x << "," << y << "," << z << endl;
 	return 0;
 }
 
-int recvRecognise(const char *strName, double score)
+int recvSwipeRecognise(const char* strName, float deltaDis)
+{
+	cout << strName << endl;
+	return 0;
+}
+
+int recvTrailRecognise(const char* strName, double score)
 {
 	cout << strName << "," << score << endl;
 	return 0;
 }
 
-
 int main()
 {
 
-	/*
-		First Way : Asynchronous call, We strongly recommend this way
-		Developer register self define function
-		recvRAW receive X, Y ,Z position of hand
-		recvRecognise receive result of hand trajectory recognizing
-		if you want to get TemplateTool, please contact me
-	*/
-	const char *strNames[] = { "Circle", "Triangle" };
-	//make sure that Circle.dat and Triangle.dat in the same directory with GestureRadarSDK.lib
-	registerLoopCallBack(recvRAW, recvRecognise, true, 2, strNames);
+	const char* strNames[] = { "Circle" };
+
+	tvt_register_loop_callBack(true, true, 1, strNames, recvRAW, recvSwipeRecognise, recvTrailRecognise);
 
 	int a;
 	cin >> a;
 
-	releaseLoopCallBack();
-	return 0;
-
-
-
-	/*
-	Second Way : Synchronous mode
-	1. inial hw
-	2. get data
-	3. release hw
-	*/
-	//if (initialHW(-1))
-	//{
-	//	while (true)
-	//	{
-	//		int x, y, reserver, z;
-	//		getRawData(x, y, reserver, z);
-	//		cout << "X: " << x << " Y:" << y << " reserve:" << reserver << " Z:" << z << endl;
-	//	}
-	//}
-	//releaseHW();
-	//getchar();
+	tvt_release_loop_callBack();
 }
--- GestureRadarDemo/GestureRadarSDK.h
+++ GestureRadarDemo/GestureRadarSDK.h
@@ -3,95 +3,111 @@
 #include <vector>
 using namespace std;
 
-//异步API，用户需要先注册回调函数
 /*
-Summary:
-Asynchronous API
-Developer should register call back function first
+Summary:异步API，用户需要先注册回调函数
 
 @param:
-callbackRawDataFunc: user define address of function，raw data function
-callbackRecognizedResultFunc: user define address of function，recognise data function
-bStartRecognizing: begin to recognizing? if true, need input callbackRecognizedResultFunc address
-strNames : recognizing data ***.dat, need our tool to establish module
+
 @return
-None
+
 */
-extern "C" void __declspec(dllexport) __stdcall registerLoopCallBack(int(*callbackRawDataFunc)(int, int, int, int), int(*callbackRecognizedResultFunc)(const char *, double), bool bStartRecognizing, int nTemplateNum, const char *strTemplateNames[]);
+extern "C" void __declspec(dllexport) __stdcall tvt_register_loop_callBack(
+	bool bRecognizeSwipe,
+	bool bRecognizeTrail,
+	int nTemplateNum,
+	const char* strTemplateNames[],
+	int(*callbackRawDataFunc)(float, float, float),
+	int(*callbackSwipeRecognizeFunc)(const char*, float),
+	int(*callbackTrailRecognizeFunc)(const char*, double)
+);
 
 
 
-//异步API，释放回调函数，释放线程资源
 /*
-Summary:
-Asynchronous API
-Realease call back function
+Summary: 释放回调函数，释放线程资源
 
 @param:
-None
+
 
 @return
-None
 */
-extern "C" void __declspec(dllexport) releaseLoopCallBack();
+extern "C" void __declspec(dllexport) tvt_release_loop_callBack();
 
-
-
-
-
-
-
-
-
-
-//同步API，找到工作的串口，并打开，不建议同步的方式
 /*
-*Summary:
-Synchronous  API
-initial serial port and find working serial port, we do not suggest use it
-
-*Parameters:
-mscTimeOut User set read microsecond timeout for gesture radar
--1 wait forever
-
-@return
-true    initial success
-false   initial failed
-*/
-extern "C" bool __declspec(dllexport) initialHW(int mscTimeOut);
-
-
-
-//同步API，阻塞式的访问
-/*
-
-*Summary:
-Synchronous  API
-get raw data from gesture radar
+Summary: 设置识别的距离
 
 @param:
-int &x 传入x地址参数 x value address
-int &y 传入y地址参数 y value address
-int &reserve 传入reserve地址参数 reserve value address
-int &z 传入z地址参数 z value address
+
 
 @return
-true    read success
-false   read failed
 */
-extern "C" bool __declspec(dllexport) getRawData(int &x, int &y, int &reserve, int &z);
+extern "C" void __declspec(dllexport) tvt_set_recognizing_range(int minZ, int maxZ);
 
 
-
-//同步API，释放串口
 /*
-Summary: Synchronous  API
-realease serial port
+Summary: 设置识别间隔时间
 
 @param:
-None
+ms 间隔时间 毫秒
 
 @return
-None
 */
-extern "C" void __declspec(dllexport) releaseHW();
+extern "C" void __declspec(dllexport) tvt_set_recognizing_interval_time(int ms);
+
+/*
+Summary: 获取识别间隔时间
+
+@param:
+ms None
+
+@return
+*/
+extern "C" int __declspec(dllexport) tvt_get_recognizing_interval_time();
+
+
+/*
+Summary: 下面都是进行识别的参数阈值
+
+@param:
+ms None
+
+@return
+*/
+extern "C" void __declspec(dllexport) tvt_set_swipe_recognizing_left_threshold(int leftThreshold);
+extern "C" int __declspec(dllexport) tvt_get_swipe_recognizing_left_threshold();
+
+extern "C" void __declspec(dllexport) tvt_set_swipe_recognizing_right_threshold(int rightThreshold);
+extern "C" int __declspec(dllexport) tvt_get_swipe_recognizing_right_threshold();
+
+extern "C" void __declspec(dllexport) tvt_set_swipe_recognizing_up_threshold(int upThreshold);
+extern "C" int __declspec(dllexport) tvt_get_swipe_recognizing_up_threshold();
+
+extern "C" void __declspec(dllexport) tvt_set_swipe_recognizing_down_threshold(int downThreshold);
+extern "C" int __declspec(dllexport) tvt_get_swipe_recognizing_down_threshold();
+
+extern "C" void __declspec(dllexport) tvt_set_swipe_recognizing_push_threshold(int pushThreshold);
+extern "C" int __declspec(dllexport) tvt_get_swipe_recognizing_push_threshold();
+
+extern "C" void __declspec(dllexport) tvt_set_swipe_recognizing_release_threshold(int releaseThreshold);
+extern "C" int __declspec(dllexport) tvt_get_swipe_recognizing_release_threshold();
+
+extern "C" void __declspec(dllexport) tvt_set_swipe_recognizing_circle_threshold(int accumulateCircle);
+extern "C" int __declspec(dllexport) tvt_get_swipe_recognizing_circle_threshold();
+
+extern "C" void __declspec(dllexport) tvt_set_swipe_recognizing_swipe_sampling_num(int swipeSamplingNum);
+extern "C" int __declspec(dllexport) tvt_get_swipe_recognizing_swipe_sampling_num();
+
+extern "C" void __declspec(dllexport) tvt_set_swipe_recognizing_drop_back_time(int millisecondDropBackTime);
+extern "C" int __declspec(dllexport) tvt_get_swipe_recognizing_drop_back_time();
+
+extern "C" bool __declspec(dllexport) tvt_write_raw_data(unsigned char* rawData, int rawDataLen);
+
+
+extern "C" void __declspec(dllexport) tvt_set_swipe_x_weight(float weight);
+extern "C" float __declspec(dllexport) tvt_get_swipe_swipe_x_weight();
+
+extern "C" void __declspec(dllexport) tvt_set_swipe_y_weight(float weight);
+extern "C" float __declspec(dllexport) tvt_get_swipe_swipe_y_weight();
+
+extern "C" void __declspec(dllexport) tvt_set_swipe_z_weight(float weight);
+extern "C" float __declspec(dllexport) tvt_get_swipe_swipe_z_weight();
